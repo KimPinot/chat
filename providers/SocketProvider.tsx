e930854857dacca783d3f924287af2871e9ff12e
nabi.lee <hello@nabi.kim>
2023-01-21 23:05:38 +0900

    Disable twice connect

--- providers/SocketProvider.tsx
+++ providers/SocketProvider.tsx
@@ -1,16 +1,25 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import useAsyncEffect from "use-async-effect";
 
 export const SocketContext = createContext<Socket | null>(null);
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
+  const initilized = useRef(false);
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useAsyncEffect(async () => {
+    if (!initilized.current || !!socket) {
+      initilized.current = true;
+      return;
+    }
     await fetch("/api/socket");
     const _socket = io();
     setSocket(_socket);
+    return () => {
+      _socket.disconnect();
+      setSocket(null);
+    };
   }, []);
 
   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
