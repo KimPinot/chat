d55131a8ac6f4f5f3c12a7040ebad7f6904cf0ba
nabi.lee <hello@nabi.kim>
2023-01-21 22:13:11 +0900

    Add simple chat feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,50 @@
 "use client";
 
-import { useEffect } from "react";
+import { Message } from "@prisma/client";
+import classcat from "classcat";
+import { FormEvent, useEffect, useState } from "react";
 
 import { useSocketProvider } from "providers/SocketProvider";
 
 export default function Home() {
   const socket = useSocketProvider();
+  const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
     socket?.on("connect", () => console.log("connected"));
+    socket?.on("message", handleNewMessage);
   }, [socket]);
 
-  return <h1>Hello World!</h1>;
+  function handleNewMessage(message: Message) {
+    setMessages((messages) => [...messages, message]);
+  }
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { value } = e.currentTarget["content"];
+    socket?.emit("message", value);
+    e.currentTarget["content"].value = "";
+  }
+
+  return (
+    <>
+      <div className="h-[calc(100vh-48px)] overflow-y-auto">
+        {messages.map((message) => (
+          <div
+            key={String(message.createdAt)}
+            className={classcat(["chat", message.author === socket?.id ? "chat-end" : "chat-start"])}
+          >
+            <div className="chat-header">{message.author}</div>
+            <div className="chat-bubble">{message.content}</div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="input-group sticky bottom-0">
+        <input type="text" name="content" placeholder="text something..." className="input-bordered input w-full" />
+        <button type="submit" className="btn btn-primary">
+          Submit!
+        </button>
+      </form>
+    </>
+  );
 }
--- socket/handler.ts
+++ socket/handler.ts
@@ -1,7 +1,19 @@
 import { Server, Socket } from "socket.io";
 
+import { database } from "utils/prisma";
+
 export function socketHandler(io: Server, socket: Socket) {
   console.log("New client connected", socket.id);
+
+  socket.on("message", async (data) => {
+    const message = await database.message.create({
+      data: {
+        content: data,
+        author: socket.id,
+      },
+    });
+    io.emit("message", message);
+  });
 
   socket.on("disconnect", () => {
     console.log("disconnected", socket.id);
